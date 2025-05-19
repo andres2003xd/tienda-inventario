5c4e2b1f3966c7558335ba858b9057677e3be485
Angel <angeltoro736@gmail.com>
2025-05-19 14:52:19 -0500

    Añadir el método existsByNombre a ProveedorRepository
    Este método permite comprobar la existencia de un proveedor por su nombre en el repositorio. Mejora las capacidades de consulta y admite casos de uso que requieren validaciones basadas en el nombre del proveedor.

--- src/main/java/com/tiendainventario/repository/ProveedorRepository.java
+++ src/main/java/com/tiendainventario/repository/ProveedorRepository.java
@@ -4,5 +4,5 @@ import com.tiendainventario.model.Proveedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProveedorRepository extends JpaRepository<Proveedor, Long> {
-
+    boolean existsByNombre(String nombre);
 }
