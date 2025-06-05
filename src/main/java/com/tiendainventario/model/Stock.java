1b1ac5be086e8b1ac9f2d7e0606f3764380192ce
ToroDevelloper <166252472+ToroDevelloper@users.noreply.github.com>
2025-06-04 22:42:10 -0500

    correccion

--- src/main/java/com/tiendainventario/model/Stock.java
+++ src/main/java/com/tiendainventario/model/Stock.java
@@ -1,49 +1,42 @@
-package com.tiendainventario.controller;
+package com.tiendainventario.model;
 
-import com.tiendainventario.model.Stock;
-import com.tiendainventario.service.StockService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
-import java.util.List;
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Stock {
 
-@RestController
-@RequestMapping("/api/stock")
-public class StockController {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_stock")
+    private Long id;
 
-    @Autowired
-    private StockService stockService;
+    @OneToOne
+    @JoinColumn(name = "id_producto", nullable = false, unique = true)
+    private Producto producto;
 
-    @GetMapping
-    public ResponseEntity<List<Stock>> listarStock() {
-        return ResponseEntity.ok(stockService.findAll());
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(nullable = false)
+    private String ubicacion;
+
+    @Column(name = "stock_minimo")
+    private Integer stockMinimo;
+
+    @Column(name = "stock_maximo")
+    private Integer stockMaximo;
+
+    public Stock(Producto producto, Integer cantidad, String ubicacion, Integer stockMinimo, Integer stockMaximo) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.ubicacion = ubicacion;
+        this.stockMinimo = stockMinimo;
+        this.stockMaximo = stockMaximo;
     }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Stock> obtenerStock(@PathVariable Long id) {
-        return ResponseEntity.ok(stockService.findById(id));
-    }
-
-    @GetMapping("/producto/{productoId}")
-    public ResponseEntity<Stock> obtenerStockPorProducto(@PathVariable Long productoId) {
-        return ResponseEntity.ok(stockService.obtenerStockPorProducto(productoId));
-    }
-
-    @PostMapping
-    public ResponseEntity<Stock> crearStock(@RequestBody Stock stock) {
-        return ResponseEntity.ok(stockService.crearStock(stock));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Stock> actualizarStock(@PathVariable Long id, @RequestBody Stock stock) {
-        return ResponseEntity.ok(stockService.actualizarStock(id, stock));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminarStock(@PathVariable Long id) {
-        stockService.delete(id);
-        return ResponseEntity.noContent().build();
-    }
-
 }
