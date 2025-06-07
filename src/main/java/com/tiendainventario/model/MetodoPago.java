58a3ddfadd7410b81834394afe82ce2726e82e52
camila340 <camilarosadita05@gmail.com>
2025-06-06 22:22:02 -0500

    SE IMPLEMENTO METODOPAGO MODEL

--- src/main/java/com/tiendainventario/model/MetodoPago.java
+++ src/main/java/com/tiendainventario/model/MetodoPago.java
@@ -21,4 +21,42 @@ public class MetodoPago {
     @OneToMany(mappedBy = "metodoPago")
     private List<Venta> ventas;
 
+    public MetodoPago(Long id, String nombre, String descripcion, List<Venta> ventas) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.ventas = ventas;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<Venta> getVentas() {
+        return ventas;
+    }
+
+    public void setVentas(List<Venta> ventas) {
+        this.ventas = ventas;
+    }
 }
