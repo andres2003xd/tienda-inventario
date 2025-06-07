a1030c3cacbf2d28e47e817a0cf514fa05fae5a9
andres2003xd <machoandres12@gmail.com>
2025-06-07 01:05:14 -0500

    get y set añadidos

--- src/main/java/com/tiendainventario/model/TipoPQRS.java
+++ src/main/java/com/tiendainventario/model/TipoPQRS.java
@@ -27,4 +27,28 @@ public class TipoPQRS {
         this.nombre = nombre;
         this.descripcion = descripcion;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 }
